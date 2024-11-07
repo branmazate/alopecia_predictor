38ad715356cafdfa31fecb638d7e97b9f2056ae9
Fermazate <fernandomazate@gmail.com>
2024-11-07 15:02:06 -0600

    ADD: pubmed_scraper.py parse_details function for parsing pubmed information.

--- src/scraping/pubmed_scraper.py
+++ src/scraping/pubmed_scraper.py
@@ -1,7 +1,9 @@
 import os
 
 from dotenv import load_dotenv
-from Bio import Entrez
+from Bio import Entrez, Medline
+import pandas as pd
+import sqlite3
 
 # Setting up the email address (required by PubMed)
 load_dotenv()
@@ -11,7 +13,7 @@ Entrez.email = email
 # Search term
 search_term = "androgenetic alopecia AND diagnosis AND progression"
 
-#Searching articles' IDs 
+#Searching articles' IDs and details
 def search_pubmed(query, retmax=100):
     handle = Entrez.esearch(db="pubmed", term=query, retmax=retmax)
     resultados = Entrez.read(handle)
@@ -23,3 +25,30 @@ def get_details(id_list):
     data = handle.read()
     handle.close()
     return data
+
+#Parssing the data
+def parse_details(data):
+    records = []
+    for record in Medline.parse(data.splitLines()):
+        #Relevant fields extractedd from the record
+            pubmed_id= record.get('PMID', ''),
+            title = record.get('TI', ''),
+            abstract = record.get('AB', ''),
+            autors = record.get('AU', ''),
+            pubdate = record.get('DP', '')
+            
+            records.append({
+                'pubmed_id': pubmed_id,
+                'title': title,
+                'abstract': abstract,
+                'autors': autors,
+                'publication date': pubdate
+            })
+    return records
+#Storing the data
+connection = sqlite3.connect('/data/raw/pubmed_data.db')
+#Ejecution
+ids = search_pubmed(search_term, retmax=1000)
+details = get_details(ids)
+regs = parse_details(details)
+df = pd.DataFrame(regs)
