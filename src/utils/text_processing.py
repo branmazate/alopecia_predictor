f8745715ffa51c32c28ec74ce83f60d1af688d94
Fermazate <fernandomazate@gmail.com>
2024-11-15 13:43:18 -0600

    ADD: get_bert_embeddings function

--- src/utils/text_processing.py
+++ src/utils/text_processing.py
@@ -2,6 +2,7 @@ import re
 import nltk
 import spacy
 from nltk.corpus import stopwords
+from transformers import AutoTokenizer, AutoModel
 
 #Download the stopwords
 nltk.download('stopwords')
@@ -32,3 +33,14 @@ def clean_text(text):
     clean_text = " ".join(final_words)
     return clean_text
 
+#Function to generate embeddings using BERT
+def get_bert_embeddings(text):
+    # Load the tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+    model = AutoModel.from_pretrained('bert-base-uncased')
+
+    # Tokenize the text
+    inputs = tokenizer(text, return_tensors='pt', truncattion=True, padding=True, max_length=128)
+    outputs = model(**inputs)
+    embeddings = outputs.last_hidden_state[:, 0, :].detach().numpy()
+    return embeddings.flatten()
